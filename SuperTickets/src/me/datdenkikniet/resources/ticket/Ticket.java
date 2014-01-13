df6b38f75ee6b2cb48d5acdc58738270e6f95bf4
Jona123 <jcdra1@gmail.com>
2014-01-13 19:58:55 +0100

    Added some comments

--- SuperTickets/src/me/datdenkikniet/resources/ticket/Ticket.java
+++ SuperTickets/src/me/datdenkikniet/resources/ticket/Ticket.java
@@ -27,42 +27,75 @@ public class Ticket {
 		this.helper = null;
 		Ticketer.ticketInstances.add(this);
 	}
-
+	/**
+	 * clear the ticket! (mark it completely as solved)
+	 */
 	public void clear() {
-		this.sender = null;
+		this.sender = "";
 		this.question = "";
 		this.helper = "";
 		this.status = Status.RESOLVED;
 	}
-
+	/**
+	 * set the helpers name
+	 * @param helper
+	 * the name of the helper
+	 */
 	public void setHelper(String helper) {
 		this.helper = helper;
 	}
-
+	/**
+	 * set the new status of a ticket
+	 * @param status
+	 * the new status
+	 */
 	public void setStatus(Status status) {
 		this.status = status;
 	}
-
+	/**
+	 * set a ticket resolved or not
+	 */
 	public void setResolved() {
 		this.status = Status.RESOLVED;
 	}
-
+	/**
+	 * get the ticket's id
+	 * @return the id of the ticket
+	 */
 	public int getId() {
 		return this.id;
 	}
-
+	/**
+	 * get the name of the sender of the ticket
+	 * @return
+	 * the name of the sender
+	 */
 	public String getSender() {
 		return this.sender;
 	}
-
+	/**
+	 * get the name of the person who is assisting the sender
+	 * @return
+	 * the name of the helper
+	 */
 	public String getHelper() {
 		return this.helper;
 	}
-
+	/**
+	 * get the question of the ticket
+	 * @return
+	 * the question of the ticket
+	 */
 	public String getQuestion() {
 		return this.question;
 	}
-
+	/**
+	 * returns wheter a player has a ticket or not
+	 * @param player 
+	 * the player to check
+	 * @return
+	 * true if the player has a ticket, false if the player doesn't
+	 */
 	public boolean ownsTicket(Player player) {
 		boolean b = false;
 		if (this.sender == player.getName()) {
--- SuperTickets/src/me/datdenkikniet/resources/ticket/Ticketer.java
+++ SuperTickets/src/me/datdenkikniet/resources/ticket/Ticketer.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class Ticketer {
 	public static ArrayList<Ticket> ticketInstances = new ArrayList<Ticket>();
-
+	/**
+	 * get all of the tickets
+	 * @return
+	 * the tickets
+	 */
 	public static ArrayList<Ticket> getTickets() {
 		return ticketInstances;
 	}
